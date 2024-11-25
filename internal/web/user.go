c718e1fbc98927738a8295dcc77fe983dca3521e
seonw <seonw@qq.com>
2024-11-25 14:57:56 +0800

    添加跨域的处理 登录的逻辑需要继续完善

--- internal/web/user.go
+++ internal/web/user.go
@@ -1,7 +1,9 @@
 package web
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-querystring/query"
 	"net/http"
 )
 
@@ -11,11 +13,12 @@ type UserHandler struct {
 func NewUserHandler() *UserHandler {
 	return &UserHandler{}
 }
+
 func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 	ug := server.Group("/users")
 	ug.POST("login", u.Login)
-	ug.GET("hello", u.Hello)
 }
+
 func (u *UserHandler) Login(ctx *gin.Context) {
 	type Req struct {
 		Code string `json:"code"`
@@ -24,8 +27,52 @@ func (u *UserHandler) Login(ctx *gin.Context) {
 	if err := ctx.Bind(&req); err != nil {
 		return
 	}
+	code2SessionReqParams := Code2SessionReqParams{
+		JsCode: req.Code,
+	}
+	code2SessionResponse := u.code2Session(&code2SessionReqParams)
+	//下面逻辑需要继续完善
+	if code2SessionResponse.SessionKey == "" {
 
+	}
+	return
 }
-func (u *UserHandler) Hello(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "hello world")
+
+type Code2SessionReqParams struct {
+	Appid     string `url:"appid"`
+	Secret    string `url:"secret"`
+	JsCode    string `url:"js_code"`
+	GrantType string `url:"grant_type"`
+}
+
+type Code2SessionResponse struct {
+	SessionKey string `json:"session_key"`
+	UnionId    string `json:"Unionid"`
+	ErrMsg     string `json:"errmsg"`
+	OpenId     string `json:"openid"`
+	ErrCode    int32  `json:"errcode"`
+}
+
+func (u *UserHandler) code2Session(reqParams *Code2SessionReqParams) *Code2SessionResponse {
+	url := "https://api.weixin.qq.com/sns/jscode2session?"
+	v, err := query.Values(reqParams)
+	if err != nil {
+		return nil
+	}
+	url += v.Encode()
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil
+	}
+	var data []byte
+	_, err = resp.Body.Read(data)
+	if err != nil {
+		return nil
+	}
+	code2SessionResponse := new(Code2SessionResponse)
+	err = json.Unmarshal(data, code2SessionResponse)
+	if err != nil {
+		return nil
+	}
+	return code2SessionResponse
 }
--- main.go
+++ main.go
@@ -1,13 +1,27 @@
-package HelloCity
+package main
 
 import (
 	"HelloCity/internal/web"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 )
 
 func main() {
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		AllowHeaders:     []string{"Content-Type"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "hellocity.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	userHandler := web.NewUserHandler()
 	userHandler.RegisterRoutes(server)
-	server.Run(":6666")
+	server.Run(":8888")
 }
